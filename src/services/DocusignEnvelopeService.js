d258f1179debf8b9afabec2ccb707708a19cb2d5
Justin Gasper <jmgasper@gmail.com>
2024-12-30 08:56:40 +1100

    Better handling of signed envelopes, to remove processing from tc-website

--- src/services/DocusignEnvelopeService.js
+++ src/services/DocusignEnvelopeService.js
@@ -62,8 +62,64 @@ create.schema = {
   }).unknown(true).required()
 }
 
-module.exports = {
-  create
+
+/**
+ * This function handles the message  for docusign envelope updates, namely when an envelope is completed.
+ * Not sure why there are two embedded "payload" objects in the message, but we'll have to roll with it
+ *
+ * @param {Object} message The kafka event message for update of a docusign envelope
+ */
+async function update (message) {
+  const payload = message.payload.payload
+
+  if(payload.status!='Completed'){
+    logger.info(`Ignoring envelope update message that does not have a "Completed" status: ${JSON.stringify(payload)}`)
+    return
+  }
+
+  // get informix connection
+  const connection = await helper.getInformixConnection()
+  try {
+    await connection.beginTransactionAsync()
+
+    // Update the terms_of_use record
+    await informixService.updateRecord(connection, InformixTableNames.DocusignEnvelope, {
+      is_completed: 1
+    }, { docusign_envelope_id: payload.envelope_id })
+
+    // commit the transaction
+    await connection.commitTransactionAsync()
+  } catch (e) {
+    logger.error(`Error in updating docusign envelope: ${JSON.stringify(e)}`)
+    await connection.rollbackTransactionAsync()
+    await helper.sendEmail(_.assign({
+      subject: config.EMAIL_SUBJECTS.DOCUSIGN_ENVELOPE,
+      toAddress: config.ERROR_EMAIL_RECIPIENT,
+      fromAddress: config.ERROR_EMAIL_SENDER,
+      message: e.message
+    }, message.payload))
+    throw e
+  } finally {
+    await connection.closeAsync()
+  }
 }
 
-// logger.buildService(module.exports)
+update.schema = {
+  message: Joi.object().keys({
+    topic: Joi.string().required(),
+    originator: Joi.string().required(),
+    timestamp: Joi.date().required(),
+    'mime-type': Joi.string().required(),
+    payload: Joi.object().keys({
+      payload: Joi.object().keys({
+        envelopeId: Joi.uuid().required(),
+        status: Joi.string().required()
+      }).unknown(true).required()
+    }).unknown(true).required()
+  }).unknown(true).required()
+}
+
+module.exports = {
+  create,
+  update
+}
